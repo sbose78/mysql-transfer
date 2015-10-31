fd8af1e67670b98b95ebe1604f9787d1f8816f4a
crowdfunding <sbose78@gmail.com>
2015-10-31 13:41:32 -0700

    Updated scripts to make a python starting point for all activities

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-from fabfile import wrapper_run_all
+#!/usr/bin/python
 
-wrapper_run_all( '192.168.43.149' , '192.168.43.148' , 'sbose' )
+
+from fabfile import wrapper_run_all
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", "--source", dest="source_database_host",
+                  help="The IP or Hostname of the database server from where database would be copied.")
+parser.add_option("-d", "--destination",dest="destination_database_host",
+                  help="The destination host where database is being copied to.")
+parser.add_option("-u", "--username",dest="username",
+                  help="The user which will be executing.")
+
+
+
+(options, args) = parser.parse_args()
+
+def copy_database(source,destination,username):
+	wrapper_run_all( source, destination , username )
+
+def main():
+	global options
+	copy_database( options.source_database_host , options.destination_database_host , options.username )
+
+main()	
