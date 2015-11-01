7eaee0d6bf2ae69d9d9d5e960f336292efecd8c9
crowdfunding <sbose78@gmail.com>
2015-11-01 00:58:39 -0700

    Script to install dependencies

---
+++ install-dependencies.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+apt-get install python
+apt-get install python-dev
+apt-get install python-setuptools  # for easy_install
+easy_install pip
+pip install fabric
